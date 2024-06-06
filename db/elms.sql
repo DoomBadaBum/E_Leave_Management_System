2b491ea76a9ddfc9f3c02a1565a67ef759f3348f
DoomBadaBum <82967719+DoomBadaBum@users.noreply.github.com>
2024-06-06 23:33:38 +0800

    Update elms.sql
    changed the data in the database

--- db/elms.sql
+++ db/elms.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jul 09, 2020 at 08:15 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.1.30
+-- Host: localhost:3306
+-- Generation Time: Jun 06, 2024 at 03:29 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,10 +28,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `UserName` varchar(100) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -40,7 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
-(1, 'admin', '827ccb0eea8a706c4c34a16891f84e7b', '2020-07-07 09:30:42');
+(1, 'admin', '827ccb0eea8a706c4c34a16891f84e7b', '2022-07-07 09:30:42');
 
 -- --------------------------------------------------------
 
@@ -49,11 +48,11 @@ INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
 --
 
 CREATE TABLE `tbldepartments` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `DepartmentName` varchar(150) DEFAULT NULL,
   `DepartmentShortName` varchar(100) NOT NULL,
   `DepartmentCode` varchar(50) DEFAULT NULL,
-  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+  `CreationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -61,9 +60,9 @@ CREATE TABLE `tbldepartments` (
 --
 
 INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `DepartmentCode`, `CreationDate`) VALUES
-(1, 'Human Resource', 'HR', 'HR001', '2017-11-01 07:16:25'),
-(2, 'Information Technology', 'IT', 'IT001', '2017-11-01 07:19:37'),
-(3, 'Operations', 'OP', 'OP1', '2017-12-02 21:28:56');
+(1, 'Human Resource', 'HR', 'HR001', '2022-11-01 07:16:25'),
+(2, 'Information Technology', 'IT', 'IT001', '2022-11-01 07:19:37'),
+(3, 'Operations', 'OP', 'OP1', '2022-12-02 21:28:56');
 
 -- --------------------------------------------------------
 
@@ -72,7 +71,7 @@ INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `De
 --
 
 CREATE TABLE `tblemployees` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `EmpId` varchar(100) NOT NULL,
   `FirstName` varchar(150) NOT NULL,
   `LastName` varchar(150) NOT NULL,
@@ -85,8 +84,8 @@ CREATE TABLE `tblemployees` (
   `City` varchar(200) NOT NULL,
   `Country` varchar(150) NOT NULL,
   `Phonenumber` char(11) NOT NULL,
-  `Status` int(1) NOT NULL,
-  `RegDate` timestamp NOT NULL DEFAULT current_timestamp()
+  `Status` int NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -94,8 +93,7 @@ CREATE TABLE `tblemployees` (
 --
 
 INSERT INTO `tblemployees` (`id`, `EmpId`, `FirstName`, `LastName`, `EmailId`, `Password`, `Gender`, `Dob`, `Department`, `Address`, `City`, `Country`, `Phonenumber`, `Status`, `RegDate`) VALUES
-(1, 'PHPTPOINT101', 'Gautam', 'Arya', 'er.gautamarya@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Male', '3 February, 1999', 'Information Technology', 'New Delhi', 'Delhi', 'India', '9608993215', 1, '2020-07-07 11:29:59'),
-(2, 'PHPTPOINT1012', 'sanjeev', 'kumar', 'phptpoint@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Male', '3 February, 1990', 'Information Technology', 'Noida ', 'Up', 'India', '8587944255', 1, '2017-11-10 13:40:02');
+(1, '01', 'Muhammad', 'Danish', 'danish@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Male', '15 May, 2001', 'Information Technology', '26, Jln Setia Impian U13/7B, Setia Alam, 40170 Shah Alam, Selangor', 'Shah Alam', 'Malaysia', '0128475844', 1, '2022-07-07 11:29:59');
 
 -- --------------------------------------------------------
 
@@ -104,17 +102,17 @@ INSERT INTO `tblemployees` (`id`, `EmpId`, `FirstName`, `LastName`, `EmailId`, `
 --
 
 CREATE TABLE `tblleaves` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `LeaveType` varchar(110) NOT NULL,
   `ToDate` varchar(120) NOT NULL,
   `FromDate` varchar(120) NOT NULL,
   `Description` mediumtext NOT NULL,
-  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
-  `AdminRemark` mediumtext DEFAULT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `AdminRemark` mediumtext,
   `AdminRemarkDate` varchar(120) DEFAULT NULL,
-  `Status` int(1) NOT NULL,
-  `IsRead` int(1) NOT NULL,
-  `empid` int(11) DEFAULT NULL
+  `Status` int NOT NULL,
+  `IsRead` int NOT NULL,
+  `empid` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -122,11 +120,9 @@ CREATE TABLE `tblleaves` (
 --
 
 INSERT INTO `tblleaves` (`id`, `LeaveType`, `ToDate`, `FromDate`, `Description`, `PostingDate`, `AdminRemark`, `AdminRemarkDate`, `Status`, `IsRead`, `empid`) VALUES
-(7, 'Casual Leave', '10/02/2020', '29/10/2020', 'test', '2020-05-01 13:11:21', 'publishing industries for ', '2017-12-02 23:26:27 ', 2, 1, 1),
-(8, 'Medical Leave test', '21/10/2020', '25/10/2020', 'test', '2020-05-01 11:14:14', 'Lorem ipsum  borum.', '2017-12-02 23:24:39 ', 1, 1, 1),
-(9, 'Medical Leave test', '08/12/2022', '12/12/2022', 'test', '2020-06-01 18:26:01', 'ok', '2020-07-07 16:13:33 ', 2, 1, 2),
-(10, 'Restricted Holiday(RH)', '25/12/2020', '25/12/2022', 'test', '2020-07-02 08:29:07', 'done', '2017-12-03 14:06:12 ', 1, 1, 1),
-(11, 'Casual Leave', '11/02/2033', '11/02/2033', 'dbdfb', '2020-07-07 10:51:37', 'goooo', '2020-07-07 16:30:52 ', 1, 1, 1);
+(12, 'Annual Leave', '13/12/2023', '12/12/2022', 'Bercuti di Pulau Langkawi', '2024-06-06 15:22:48', 'Not approved due to late notice of annual leave', '2024-06-06 20:55:10 ', 2, 1, 1),
+(13, 'Medical Leave', '20/12/2022', '19/12/2022', 'Cuti Sakit Dr Bagi MC', '2024-06-06 15:23:16', NULL, NULL, 0, 0, 1),
+(14, 'Emergency Leave', '25/12/2022', '24/12/2022', 'Kereta rosak', '2024-06-06 15:23:50', 'Approved due to emergency', '2024-06-06 20:56:50 ', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -135,10 +131,10 @@ INSERT INTO `tblleaves` (`id`, `LeaveType`, `ToDate`, `FromDate`, `Description`,
 --
 
 CREATE TABLE `tblleavetype` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `LeaveType` varchar(200) DEFAULT NULL,
-  `Description` mediumtext DEFAULT NULL,
-  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp()
+  `Description` mediumtext,
+  `CreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -146,9 +142,9 @@ CREATE TABLE `tblleavetype` (
 --
 
 INSERT INTO `tblleavetype` (`id`, `LeaveType`, `Description`, `CreationDate`) VALUES
-(1, 'Casual Leave', 'Casual Leave ', '2017-11-01 12:07:56'),
-(2, 'Medical Leave test', 'Medical Leave  test', '2017-11-06 13:16:09'),
-(3, 'Restricted Holiday(RH)', 'Restricted Holiday(RH)', '2017-11-06 13:16:38');
+(1, 'Annual Leave', 'Annual Leave ', '2022-11-01 12:07:56'),
+(2, 'Medical Leave', 'Medical Leave', '2022-11-06 13:16:09'),
+(3, 'Emergency Leave', 'Emergency Leave', '2022-11-06 13:16:38');
 
 --
 -- Indexes for dumped tables
@@ -193,31 +189,31 @@ ALTER TABLE `tblleavetype`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbldepartments`
 --
 ALTER TABLE `tbldepartments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tblemployees`
 --
 ALTER TABLE `tblemployees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tblleaves`
 --
 ALTER TABLE `tblleaves`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tblleavetype`
 --
 ALTER TABLE `tblleavetype`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
